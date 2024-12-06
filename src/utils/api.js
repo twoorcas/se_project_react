32b29d3f724a4503fa3bba543ba91dad5f149c28
Julie Chen <juliechenmedicine@gmail.com>
2024-12-05 20:17:06 -0800

    frontend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwrbyjulie.twilightparadox.com"
+    : "http://localhost:3001";
 
 function getResult(res) {
   if (res.ok) {
